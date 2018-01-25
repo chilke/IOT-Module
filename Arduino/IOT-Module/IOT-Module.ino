b7ddeb5c526e2beac28da713f93481d98c8317e5
Craig Hilke <craig_ka@yahoo.com>
2018-01-24 21:23:56 -0600

    refactor complete

--- Arduino/IOT-Module/IOT-Module.ino
+++ Arduino/IOT-Module/IOT-Module.ino
@@ -1,4 +1,15 @@
-#include "ESP8266WiFi.h"
+#include <WiFiClientSecure.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiUdp.h>
+#include <ESP8266WiFiType.h>
+#include <ESP8266WiFiAP.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiSTA.h>
+
 #include "UDPListener.h"
 #include "OTA.h"
 #include "limits.h"
